daf1f3185c6a6574a8e91fbb834f9544adc9a423
BonelessSugar <106636487+BonelessSugar@users.noreply.github.com>
2022-07-02 17:28:35 -0400

    Create InvalidPetException.java

---
+++ HW6/InvalidPetException.java
@@ -0,0 +1,8 @@
+public class InvalidPetException extends RuntimeException {
+	public InvalidPetException() {
+		this("Your pet is invalid!");
+	}
+	public InvalidPetException(String s) {
+		super(s);
+	}
+}
